2fe187ef5533389cb33ec061a273550ed429ede0
saadTkxon <saad.tkxon@gmail.com>
2025-07-15 16:59:44 +0500

    --data types escaped characters...type casting and its type..why we need type casting

---
+++ basics/2numbersum.py
@@ -0,0 +1,32 @@
+num1=input("enter the 1st number")
+num2=input("enter teh 2nd number")
+print(num1+num2)
+
+# the above is print the result as stering addition because it by default the string
+
+# full accrute iwth the type casting code
+num1=int(input("enter the 1st number"))
+num2=int(input("enter teh 2nd number"))
+print(num1+num2)
+
+"""
+type casting has 2 types:
+implict type casting
+excplict type casitng
+"""
+
+
+"""
+
+escaped characters:"ued for pirnt the charecaters non printable or sepceall one or owth our one indentation then we sued that"
+\n:new line
+\t:tab
+\\:backslash
+\":double quote
+':single quote
+\b:backspace
+\f:form feed
+\r:carriage return
+\a:bell
+
+"""
---
+++ basics/datatypes.py
@@ -0,0 +1,59 @@
+"""
+data types in python:
+1)numeric
+.int
+.float
+.complex
+
+2)text type
+.str
+
+3)boolean
+.bool
+
+4)sequence type
+.list
+.tuple
+.range
+
+5)maping type
+.dict
+
+6)set type
+.set
+
+7)binary type
+.bytes
+.bytearray
+
+8)none type
+ 
+
+"""
+
+
+# int
+age=32
+# float
+weight=43.20
+# string
+name='saad'
+# boolean
+is_adult=True
+# list
+fruits=['apple','banana','cherry']
+# dictonary
+dict1={'name':'saad','age':'10','city':'lahore'}
+# tupple
+tup=(10,20,20)
+# set
+set1={10,20,30,40,50}
+# bytes
+byte=b'hello'
+# bytearray
+bytearray1=b'hello'
+# none
+none=None
+# print
+print('int:',age)
+print('float:',weight)
