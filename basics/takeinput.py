9bff9789d3d182512f9678997300ae6e3b148e3c
saadTkxon <saad.tkxon@gmail.com>
2025-07-15 16:39:35 +0500

    --input and how can we deal with that and by default input type if not specfied

---
+++ basics/takeinput.py
@@ -0,0 +1,27 @@
+# how to take input in python
+
+name = input("enter your name: ")
+print("hello this is just for practice and i want to just save rgar", name)
+
+
+
+# take the number in input form the user and pirnt its type as well
+
+num = (input("enter any number : "))
+print(num)
+print("type of number is : ", type(num))
+
+
+# if we not specify the type the this will by default send the type string
+
+"""
+output of the code:
+saad@saad-ThinkPad-T431s:~/Documents/Developer/python-basics-to-advanced/basics$ p
+ython3 takeinput.py
+enter your name: hasl
+hello this is just for practice and i want to just save rgar hasl
+enter any number : 1
+1
+type of number is :  <class 'str'>
+saad@saad-ThinkPad-T431s:~/Documents/Developer/python-basics-to-advanced/basics$ 
+"""
